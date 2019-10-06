ba744b04ce70149cae9603f588b8bc96714b1366
Diana M <dmmiller85@gmail.com>
2019-10-06 18:48:15 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,9 @@
 # Your code here
+def meal_choice(veg1, veg2, protein = "meat")
+  if protein = nil
+    protein = "meat"
+  else protein = protein
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+meal_choice
